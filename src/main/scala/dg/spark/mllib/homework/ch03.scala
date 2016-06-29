1d6c0a222625e841ba4b2d2b06b6d316ccd12283
xenron <xenron@hotmail.com>
2016-06-29 23:11:29 +0900

    msg

--- src/main/scala/dg/spark/mllib/homework/ch03.scala
+++ src/main/scala/dg/spark/mllib/homework/ch03.scala
@@ -3,6 +3,7 @@ package dg.spark.mllib.homework
 import java.text.SimpleDateFormat
 import java.util.Date
 
+import breeze.linalg.{max, min}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
@@ -15,21 +16,26 @@ object LinearRegression {
     Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
     // 设置运行环境
-    val conf = new SparkConf().setAppName(this.getClass().getSimpleName().filter(!_.equals('$'))).setMaster("local[4]")
+//    val conf = new SparkConf().setAppName(this.getClass().getSimpleName().filter(!_.equals('$'))).setMaster("local[4]")
+    val conf = new SparkConf().setAppName("Week3")
     val sc = new SparkContext(conf)
 
     // Load and parse the data
-    val data = sc.textFile("file/data/mllib/input/ridge-data/lpsa.data",1)
-    //如果读入不加1，会产生两个文件，应该是默认生成了两个partition
-    val parsedData = data.map { line =>
+    // 如果读入不加1，会产生两个文件，应该是默认生成了两个partition
+    val data = sc.textFile("/tmp/lpsa.data",1)
+
+    var parsedData = data.map { line =>
       val parts = line.split(',')
       LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
     }
-
+    // 标准化归一化
+    val standardData = parsedData.map {
+      line => LabeledPoint(line.label, Vectors.dense(standard(line.features.toArray)))
+    }
     // Building the model
     //建立model的数据和predict的数据没有分开
     val numIterations = 100
-    val model = LinearRegressionWithSGD.train(parsedData, numIterations)
+    val model = LinearRegressionWithSGD.train(standardData, numIterations)
     //    for(i<-parsedData) println(i.label+":"+i.features);
     // Evaluate model on training examples and compute training error
     val valuesAndPreds = parsedData.map { point =>
@@ -41,12 +47,30 @@ object LinearRegression {
     println("model.weights"+weifhts)
 
     //save as file
-    val iString = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date())
-    val path = "file/data/mllib/output/LinearRegression/" + iString + "/result"
-    valuesAndPreds.saveAsTextFile(path)
+//    val iString = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date())
+//    val path = "file/data/mllib/output/LinearRegression/" + iString + "/result"
+//    valuesAndPreds.saveAsTextFile(path)
     val MSE = valuesAndPreds.map { case (v, p) => math.pow((v - p), 2) }.reduce(_ + _) / valuesAndPreds.count
     println("training Mean Squared Error = " + MSE)
 
     sc.stop()
+  }
+
+  /**
+    * 数组标准化
+    */
+  def standard(x: Seq[Double]): Array[Double] = {
+    val s = new Array[Double](x.length)
+    val avg = x.sum / x.length
+    var variance = 0.0
+    for (i <- 0 until x.length) {
+      variance += math.pow(x(i) - avg, 2)
+    }
+    val std = math.sqrt(variance / (x.length - 1.0))
+    // 样本标准偏差
+    for (i <- 0 until x.length) {
+      s(i) = (x(i) - avg) / std
+    }
+    return s
   }
 }
