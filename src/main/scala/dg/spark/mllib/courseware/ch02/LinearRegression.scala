fe83bedce303c453fddd00d2da6f6d865c017131
xenron <xenron@hotmail.com>
2016-07-16 23:33:27 +0900

    msg

---
+++ src/main/scala/dg/spark/mllib/courseware/ch02/LinearRegression.scala
@@ -0,0 +1,9 @@
+package dg.spark.mllib.courseware
+
+import org.apache.spark.SparkConf
+
+object LinearRegression {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("test")
+  }
+}
